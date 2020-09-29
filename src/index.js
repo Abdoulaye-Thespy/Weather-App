0ada07402497611ad3781e8df3417072275e0934
Abdoulaye-Thespy <njigouhrazak@iut-dhaka.edu>
2020-09-29 19:14:40 +0100

    added function to hit the API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-alert('intial setup');
+  async function getCats() {
+    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=e099642ba8b7f8b636938fb26d4c6038', {mode: 'cors'});
+    const weatherData = await response.json();
+     console.log(weatherData);
+  }
+
+  getCats();
