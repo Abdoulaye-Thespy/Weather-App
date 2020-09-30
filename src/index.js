629c7eba77ad9069fa48e2dd673a6ae917702cb5
Abdoulaye-Thespy <njigouhrazak@iut-dhaka.edu>
2020-09-30 20:30:19 +0100

    got date needed from JSON

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+ 
+const searchBtn = document.getElementById('sbtn');
+
   async function getWeather(town) {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${town}&appid=e099642ba8b7f8b636938fb26d4c6038`, {mode: 'cors'});
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${town}&appid=e099642ba8b7f8b636938fb26d4c6038&units=metric `, {mode: 'cors'});
     const weatherData = await response.json();
      console.log(weatherData);
      console.log('seperate');
@@ -7,10 +10,24 @@
   }
 
 
- async function findWeather () {
-	const twonWeather = await getWeather('London');
-	console.log(twonWeather.main.temp);
+const town = () => {
+	const town = document.getElementById('inp').value;
+	return town;
+}
+
+
+ async function findWeather (e) {
+ 	e.preventDefault()
+ 	const towns =town();
+	const twonWeather = await getWeather(towns);
+
+	const townName = twonWeather.name;
+	const townTemp = twonWeather.main.temp;
+	const townMain = twonWeather.weather[0].main;
+	const townDesc = twonWeather.weather[0].description;
+	console.log (townName, townTemp, townMain, townDesc);
 
 }
 
-findWeather();
+
+searchBtn.addEventListener('click', findWeather);
