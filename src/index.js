608abff9de133045310bfd6bfdc22a939bc9cd11
Abdoulaye-Thespy <njigouhrazak@iut-dhaka.edu>
2020-09-30 16:11:31 +0100

    done with the design of the main page of our app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
-  async function getCats() {
-    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=e099642ba8b7f8b636938fb26d4c6038', {mode: 'cors'});
+  async function getWeather(town) {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${town}&appid=e099642ba8b7f8b636938fb26d4c6038`, {mode: 'cors'});
     const weatherData = await response.json();
      console.log(weatherData);
+     console.log('seperate');
+     return weatherData;
   }
 
-  getCats();
+
+ async function findWeather () {
+	const twonWeather = await getWeather('London');
+	console.log(twonWeather.main.temp);
+
+}
+
+findWeather();
